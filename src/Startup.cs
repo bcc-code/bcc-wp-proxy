2d32fe1c30a314354b6d520b0cf1b77839fb702d
Reng van Oord <reng.vanoord@oslofjord.com>
2021-05-28 07:41:52 +0200

    Attempt to fix IOS login issues

--- src/Startup.cs
+++ src/Startup.cs
@@ -93,10 +93,9 @@ namespace bcc_wp_proxy
 
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.None;
-                options.Secure = CookieSecurePolicy.Always;
+                options.MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.Unspecified;
+                options.OnAppendCookie = cookieContext => CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                options.OnDeleteCookie = cookieContext => CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
             });
 
             services.AddAuthentication(options =>
@@ -192,6 +191,66 @@ namespace bcc_wp_proxy
             services.AddSingleton<WPMessageHandler>();
             services.AddSingleton<WPMessageInvokerFactory>();
             services.AddApplicationInsightsTelemetry();
+        }
+
+        //Ref: https://programmer.group/asp.net-core-3.1-browser-sniffing-solves-missing-cookies-in-some-browsers.html
+
+        private void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+
+            if (options.SameSite == Microsoft.AspNetCore.Http.SameSiteMode.None)
+            {
+                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+                if (UserAgentDisallowsSameSiteNone(userAgent))
+                {
+                    options.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Unspecified;
+                }
+
+            }
+        }
+
+        private static bool UserAgentDisallowsSameSiteNone(string userAgent)
+        {
+
+            // Check if a null or empty string has been passed in, since this
+            // will cause further interrogation of the useragent to fail.
+            if (String.IsNullOrWhiteSpace(userAgent))
+                return false;
+
+            // Cover all iOS based browsers here. This includes:
+            // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+            // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+            // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+            // All of which are broken by SameSite=None, because they use the iOS networking
+            // stack.
+            if (userAgent.Contains("CPU iPhone OS 12") ||
+                userAgent.Contains("iPad; CPU OS 12"))
+            {
+                return true;
+            }
+
+            // Cover Mac OS X based browsers that use the Mac OS networking stack. 
+            // This includes:
+            // - Safari on Mac OS X.
+            // This does not include:
+            // - Chrome on Mac OS X
+            // Because they do not use the Mac OS networking stack.
+            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
+                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+            {
+                return true;
+            }
+
+            // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
+            // and none in this range require it.
+            // Note: this covers some pre-Chromium Edge versions, 
+            // but pre-Chromium Edge does not require SameSite=None.
+            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
